1594871093a345c6240aff6af3f4a35b04c423a8
Jon Zenor <JonZenor@gmail.com>
2021-12-06 20:27:31 -0700

    Initial commit after doing some work on things.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace makeItWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int ChildTckts, AdultTckts, TotalTckts, TotalPrice;
+
+            ChildTckts = 2;
+            AdultTckts = 3;
+            TotalTckts = ChildTckts + AdultTckts;
+
+            int BaseTicketPrice = 10;
+
+            TotalPrice = (AdultTckts * BaseTicketPrice);
+            TotalPrice += (ChildTckts * (BaseTicketPrice -3));
+
+            Console.Write(AdultTckts + " Adult tickets + " + ChildTckts + " Child Tickets = $" + TotalPrice);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# My C# Notes #
+
+1. Concatinate with PLUS: var + var
+
+2. Plus Equals to add to a variable: TotalPrice += tax;
+
+
+## Start A Project ##
+
+In VS Code:
+
+In the Create a new project window, select All languages, and then choose C# from the dropdown list. Choose Windows from the All platforms list, and choose Console from the All project types list.
+
+After you apply the language, platform, and project type filters, choose the Console App template, and then select Next.
