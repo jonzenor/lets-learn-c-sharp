550f1c8769e339836da7ff39701f78d83b3ed6a9
Jon Zenor <JonZenor@gmail.com>
2021-12-15 19:32:05 -0700

    Added a bunch of fun number tricks.

--- Program.cs
+++ Program.cs
@@ -2,16 +2,16 @@
 
 namespace makeItWork
 {
-    /***********************************************************************
-        I'm creating this to test out the different code examples given in
-        Programming in the Key of C# by Charles Petzold
-    ***********************************************************************/
+    // -----------------------------------------------------------------------
+    // I'm creating this to test out the different code examples given in
+    // Programming in the Key of C# by Charles Petzold
+    // -----------------------------------------------------------------------
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+//            Console.WriteLine("Hello World!");
+/*
             int ChildTckts, AdultTckts, TotalTckts, TotalPrice;
 
             ChildTckts = 2;
@@ -24,12 +24,57 @@ namespace makeItWork
             TotalPrice += (ChildTckts * (BaseTicketPrice -3));
 
             Console.Write(AdultTckts + " Adult tickets + " + ChildTckts + " Child Tickets = $" + TotalPrice);
-
+*/
             // Comments are the standard fare. You have this kind
             /* And this kind. */
             /*
                 # This kind of comment does not work, though.
             */
+
+            // See how high C# can count to.
+            // uint X = 2;
+
+            // Console.WriteLine(X);
+            // Console.WriteLine(checked(X *= X));
+            // Console.WriteLine(checked(X *= X));
+            // Console.WriteLine(checked(X *= X));
+            // Console.WriteLine(checked(X *= X));
+            // Console.WriteLine(checked(X *= X - 1));
+            // Console.WriteLine(checked(X *= X));
+            // Console.WriteLine(checked(X *= X));
+            // Console.WriteLine(checked(X *= X));
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+            // Console.WriteLine(X *= X);
+
+            // Let's see the top and bottom value for a signed integer
+            Console.WriteLine("Signed");
+            int Signed = 2147483647;
+            int SignedZero = 0;
+
+            Console.Write(Signed + " + 1 = ");
+            Console.WriteLine(Signed + 1);
+            
+            Console.Write(SignedZero + " - 1 = ");
+            Console.WriteLine(SignedZero - 1);
+
+            Console.WriteLine();
+            Console.WriteLine("Unsigned");
+            uint Unsigned = 2147483647;
+            uint UnsignedZero = 0;
+
+            Console.Write(Unsigned + " + 1 = ");
+            Console.WriteLine(Unsigned +1 );
+
+            Console.Write(UnsignedZero + " - 1 = ");
+            Console.WriteLine(UnsignedZero - 1);
+
         }
     }
 }
